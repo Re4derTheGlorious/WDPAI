6b30e6f68bfbf541390f8c9ecfd296e4085a7ea5
Re4derTheGlorious <karol.re4der@gmail.com>
2021-12-07 22:10:47 +0100

    Fixed css sheet not loading, expanded html and php part

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Hello there";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('gallery', 'DefaultController');
+
+Router::run($path);
---
+++ public/views/gallery.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/style.css">
+    <title>re4tography</title>
+</head>
+<body>
+    <div id="contents_panel"></div>
+    <div id="fade" style="display:none;"></div>
+    <div id="buttons_panel">
+        <button class="button_icon" style="display:none;">
+            <img src="public/res/icons/Icon_Share.svg">
+        </button>
+        <button class="button_icon" style="display:none;">
+            <img src="public/res/icons/Icon_Fav.svg">
+        </button>
+        <button class="button_icon">
+            <img src="public/res/icons/Icon_User.svg">
+        </button>
+        <button class="button_icon">
+            <img src="public/res/icons/Icon_Info.svg">
+        </button>
+    </div>
+    <div id="navigation_panel">
+        <div id="left_nav_panel" class="nav_panel">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="nav_symbol">
+                <polygon points="0,15 30,30 30,0"/>
+            </svg>
+        </div>
+        <div id="right_nav_panel" class="nav_panel">
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="nav_symbol">
+                <polygon points="30,15 0,30 0,0"/>
+            </svg>
+        </div>
+        <div id="bottom_nav_panel">
+            <span class="nav_ball" style="opacity:0.2; width:15px; height: 15px"></span>
+            <span class="nav_ball" style="opacity:0.4; width:22px; height: 22px"></span>
+            <span class="nav_ball" style="opacity:0.6; width:30px; height: 30px"></span>
+            <span class="nav_ball"></span>
+            <span class="nav_ball" style="opacity:0.6; width:30px; height: 30px"></span>
+            <span class="nav_ball" style="opacity:0.4; width:22px; height: 22px"></span>
+            <span class="nav_ball" style="opacity:0.2; width:15px; height: 15px"></span>
+        </div>
+    </div>
+    <div id="about_panel" class="panel" style="display:none;">
+        <div class="text_title">About me</div>
+        <div class="text_panel">
+            <p>&emsp;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed mattis orci. Mauris sapien nulla, ultrices id est in, fermentum iaculis massa. Integer interdum mollis ligula, eget posuere elit luctus nec. Maecenas egestas elit eget odio commodo, dictum iaculis sem vulputate. Etiam sollicitudin euismod laoreet. Donec sed ullamcorper augue, et varius dui. Sed euismod metus vitae massa auctor aliquet.</p>
+            <p>&emsp;Aenean eu malesuada massa, vitae sodales leo. Maecenas ac tellus molestie, efficitur purus vel, suscipit tellus. Proin convallis imperdiet elit, vitae consequat ligula tempor in. Quisque tempor ullamcorper velit, in vulputate ligula pharetra at. Morbi sodales augue at lacus molestie, eget condimentum nisi tincidunt. Mauris arcu metus, cursus vitae felis ac, pulvinar euismod nunc. Curabitur pharetra magna tellus, et tempor dolor sollicitudin eu. Integer mollis magna vel euismod tincidunt. Aliquam convallis, ante eget finibus hendrerit, mauris purus convallis lorem, et lobortis felis tellus non ex. Fusce consectetur tortor pulvinar nisi luctus, a facilisis justo pretium. Quisque dolor nibh, hendrerit quis hendrerit gravida, rutrum ut diam. Vivamus tincidunt purus sed sem placerat, eu dignissim nisi interdum. Aenean dictum vehicula consectetur. Pellentesque tempor nibh nec orci porttitor fringilla. Proin ut quam nunc. Nunc sed quam lobortis, mattis felis sed, condimentum ipsum.</p>
+            <p>&emsp;Quisque aliquet quam ut odio congue varius. In sed elit bibendum, rutrum mauris ac, egestas nulla. Mauris vel felis efficitur, sagittis orci sollicitudin, placerat risus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce blandit vestibulum massa, vitae sollicitudin odio scelerisque vitae. Vivamus elementum vehicula massa. Mauris et aliquet velit, eget tincidunt velit.</p>
+            <p>&emsp;Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In congue, felis et posuere semper, eros arcu feugiat lacus, fermentum commodo felis magna id mi. Nulla venenatis aliquam lorem sed varius. Proin ultricies lacus sed elit posuere, id maximus urna venenatis. Aenean at est dolor. Morbi ac sodales ex. Curabitur fringilla justo vel neque venenatis dictum. Nulla sed sagittis diam, ac dapibus diam. Quisque cursus fringilla velit, quis laoreet purus venenatis convallis. Mauris et est ac nisl aliquam euismod. Sed nec iaculis tortor. Praesent vulputate ut eros ut scelerisque. Nulla sollicitudin sodales mollis. Phasellus molestie, enim non suscipit pellentesque, est lectus interdum nunc, in sagittis elit tortor ut lorem.</p>
+            <p>&emsp;Duis ultrices quam non metus vehicula vulputate. Praesent eget ante sit amet sem ullamcorper pulvinar eget sed arcu. Quisque fringilla vel turpis condimentum hendrerit. Donec vel ligula diam. Aliquam elementum augue massa, at convallis dui dapibus quis. Nam commodo finibus lorem, vitae eleifend nisi. Ut ex dolor, porttitor sed massa sit amet, suscipit mollis mauris.</p>
+        </div>
+    </div>
+    <div id="user_panel", class="panel", style="display:initial;">
+        <div class="text_title">User</div>
+        <form>
+            <input name="login_field" type="text" placeholder="Your Email">
+            <input name="pass_field" type="password" placeholder="Your Password">
+            <div>
+                <button>Sign In</button>
+                <button>Sign Up</button>
+            </div>
+        </form>
+    </div>
+</body>
